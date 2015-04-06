8aa52fe1ec461c2a0402f6144ef80df1d47a11fd
kennyazrina <kaniaazrina@gmail.com>
2015-04-06 15:12:15 -0400

    Web App
    Basemap + input field for searching location

---
+++ WebApp/map.js
@@ -0,0 +1,11 @@
+function initialize() {
+  var mapOptions = {
+    center: { lat: 40.725497, lng: -73.974016},
+    zoom: 14,
+    disableDefaultUI: true,
+    styles :[{"stylers":[{"hue":"#ff1a00"},{"invert_lightness":true},{"saturation":-100},{"lightness":33},{"gamma":0.5}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2D333C"}]}]
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
