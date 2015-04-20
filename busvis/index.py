9849ae2b861ac07dd04baef271ab9e08fe4e58e4
kennyazrina <kaniaazrina@gmail.com>
2015-04-20 13:00:49 -0400

    changed gmaps to mapbox

--- busvis/index.py
+++ busvis/index.py
@@ -30,18 +30,23 @@ def index():
 
 	import psycopg2
 	#date=raw_input('enter date: (2014-08-01)\n').strip()
-	try:
-		conn=psycopg2.connect("dbname='bushistorical' user='postgres' host='busvis.cloudapp.net'")
-		cur=conn.cursor();
-		# cur.execute("""SELECT COUNT(*)FROM(SELECT busid,COUNT(*) bus_count FROM bus_test WHERE to_char(TIMESTAMP,'YYYY-MM-DD') ='%s' GROUP BY busid) AS table1"""%(date))
-		cur.execute("SELECT COUNT(DISTINCT busid) FROM bus_test")
-		rows=cur.fetchall()
-		totalBusCount = int(rows[0][0])
 
-	except:
+	#commented for development
+	#try:
+		conn=psycopg2.connect("dbname='bushistorical' user='postgres' host='busvis.cloudapp.net'")
+	#	cur=conn.cursor();
+		# cur.execute("""SELECT COUNT(*)FROM(SELECT busid,COUNT(*) bus_count FROM bus_test WHERE to_char(TIMESTAMP,'YYYY-MM-DD') ='%s' GROUP BY busid) AS table1"""%(date))
+	#	cur.execute("SELECT COUNT(DISTINCT busid) FROM bus_test")
+	#	rows=cur.fetchall()
+	#	totalBusCount = int(rows[0][0])
+
+	#except:
 		## if not able to connect to DB use static fake number - TODO: change to error message
 		# print '<html><body><p>could not connect to database</p></body></html>'
-		totalBusCount = 10000
+		#totalBusCount = 10000
+
+	#new line after development - remove after finish
+	totalBusCount = 10000
 
 	## send variable buscount to be shown inside /templates/index.html
 	#return '<html><body><p>see what we got: ' + str(int(rows[0][0])) + '</p></body></html>'
