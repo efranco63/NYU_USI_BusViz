5051acf33543a1914c5c8a98feac1e116c619b0f
JiaminXuan <JiaminXuan@users.noreply.github.com>
2015-05-05 17:01:01 -0400

    Update index.py

--- busvis/index.py
+++ busvis/index.py
@@ -32,7 +32,7 @@ def get_waittimes():
 	# pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
 	# r = redis.Redis(connection_pool=pool)
 	# stop_id_json=r.hget('stopid',stop_id)
-	return jsonify(stop_id_json)
+	#return jsonify(stop_id_json)
 @app.route('/')
 def index():
 	date = "2014-08-04"
