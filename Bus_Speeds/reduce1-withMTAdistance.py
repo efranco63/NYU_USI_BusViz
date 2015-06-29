b911101b81cd0a13dc1063e7434d36a69f12765d
dimbi <dimasrinarso2003@yahoo.com>
2015-06-29 07:44:46 -0400

    bus speed for all routes trial 1

--- Bus_Speeds/reduce1-withMTAdistance.py
+++ Bus_Speeds/reduce1-withMTAdistance.py
@@ -9,7 +9,6 @@
 import sys
 from datetime import datetime
 
-
 def main():
 
     start_point = None
@@ -36,40 +35,40 @@ def main():
         status = key[5]
         stop = values[4:]
 
-        if route == 'MTA NYCT_B38' and direction == '0':
+        #if route == 'MTA NYCT_B38' and direction == '0':
 
-            if (busid == prior_id) and (direction == prior_dir) and (route == prior_route):
-                if stop != 'NULL' and stop != '' and stop != prior_stop:
-                    if start_point != None:
-                        end_point = prior_time
-                        end_distance = prior_distance
-                        delta_time = (end_point-start_point).total_seconds()
-                        delta_distance = float(start_distance) - float(end_distance)
-                        if delta_time != 0 and delta_distance > 0:
-                            print "%s|%s|%s\t%s\t%s\t" % (prior_route, prior_stop, prior_dir, str(delta_time), str(delta_distance))
+        if (busid == prior_id) and (direction == prior_dir) and (route == prior_route):
+            if stop != 'NULL' and stop != '' and stop != prior_stop:
+                if start_point != None:
+                    end_point = prior_time
+                    end_distance = prior_distance
+                    delta_time = (end_point-start_point).total_seconds()
+                    delta_distance = float(start_distance) - float(end_distance)
+                    if delta_time != 0 and delta_distance > 0:
+                        print "%s|%s|%s\t%s\t%s\t" % (prior_route, prior_stop, prior_dir, str(delta_time/3600), str(delta_distance/1000))
                             #print "%s|%s|%s\t%s\t%s\t%s,%s,%s" % (prior_route, prior_stop, prior_dir, str(delta_time), str(delta_distance),\
                             #                              start_point.strftime("%Y-%m-%d %H:%M:%S"), end_point.strftime("%Y-%m-%d %H:%M:%S"), status)
-                    start_point = time_stamp
-                    start_distance = distance
-                else:
-                    if stop == 'NULL':
-                        start_point = None
-                        end_point = None
-                        start_distance = None
-                        end_distance = None
+                start_point = time_stamp
+                start_distance = distance
             else:
-                start_point = None
-                end_point = None
-                start_distance = None
-                end_distance = None
-
-            prior_id = busid
-            prior_dir = direction
-            prior_route = route
-            prior_stop = stop
-            prior_time = time_stamp
-            prior_distance = distance
-
+                if stop == 'NULL':
+                    start_point = None
+                    end_point = None
+                    start_distance = None
+                    end_distance = None
+        else:
+            start_point = None
+            end_point = None
+            start_distance = None
+            end_distance = None
+ 
+        prior_id = busid
+        prior_dir = direction
+        prior_route = route
+        prior_stop = stop
+        prior_time = time_stamp
+        prior_distance = distance
+ 
 if __name__ == "__main__":
     main()
 
